86d37ae8464bb9ac67e0885877e5686e9d28ed31
Obaada <159489903+Obaada@users.noreply.github.com>
2024-02-24 00:33:14 +0000

    done

--- main.py
+++ main.py
@@ -8,3 +8,41 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Local Scope
+def drink_potion():
+  potion_strength = 2 
+  print(potion_strength)
+
+drink_potion()
+#print(potion_strength)
+
+# Global Scope
+player_health = 10
+
+def drink_potion():
+  potion_strength = 2 
+  print(player_health)
+
+drink_potion()
+print(player_health)
+
+#There is no Block Scope
+game_level = 3
+enemies = ["Skeleton", "Zombies", "Alien"]
+if game_level < 5:
+  new_enemy = enemies[0]
+  
+print(new_enemy)
+
+# Modifying Global scope
+
+enemies = 1
+
+def increase_enemies():
+  global enemies
+  enemies = 2
+  print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
